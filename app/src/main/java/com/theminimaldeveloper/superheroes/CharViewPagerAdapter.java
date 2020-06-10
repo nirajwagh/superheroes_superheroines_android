00a106e7b6d355fc6c19f567b99e8175df56ccac
nirajwagh <iamnirajwagh@gmail.com>
2020-06-10 23:49:38 +0530

    Superheroes added

---
+++ app/src/main/java/com/theminimaldeveloper/superheroes/CharViewPagerAdapter.java
@@ -0,0 +1,88 @@
+package com.theminimaldeveloper.superheroes;
+
+import android.content.Context;
+import android.media.Image;
+import android.text.Layout;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.viewpager.widget.PagerAdapter;
+
+import java.util.Arrays;
+
+public class CharViewPagerAdapter extends PagerAdapter {
+
+     Context context;
+    private String[] charNames;
+    private int[] imageIds;
+     LayoutInflater layoutInflater;
+
+
+    public CharViewPagerAdapter(Context context, String[] charNames, int[] imageIds) {
+        this.context = context;
+        this.charNames = charNames;
+        this.imageIds = imageIds;
+
+        layoutInflater =  (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+//        Log.d("\n\nimageeeeee", imageIds[1]+ "");
+//        Log.d("\n\nimageeeeee111", charNames[1]);
+    }
+
+
+
+
+    @Override
+    public int getCount() {
+        return imageIds.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == ((ConstraintLayout)object);
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, final int position) {
+
+
+
+        View view = layoutInflater.inflate(R.layout.char_vp_item, container ,false);
+
+        ImageView img_char = (ImageView) view.findViewById(R.id.img_char);
+        TextView txt_char_name = (TextView) view.findViewById(R.id.txt_char_name);
+
+
+
+        Log.d("\n\npos", position+"");
+
+        img_char.setImageResource(imageIds[position]);
+        txt_char_name.setText(charNames[position]);
+
+
+        container.addView(view);
+
+        img_char.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, charNames[position], Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
+    }
+
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((ConstraintLayout) object);
+    }
+}
---
+++ app/src/main/java/com/theminimaldeveloper/superheroes/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.theminimaldeveloper.superheroes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, CharactersActivity.class);
+                startActivity(intent);
+            }
+        }, 2500);
+    }
+}
